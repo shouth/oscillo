eec48c40dccf7d139ae6625f546dab09fbd450d9
Shota Minami <shouth373@gmail.com>
2024-05-19 18:33:22 +0900

    Refactor

--- oscelas/src/char_set.rs
+++ oscelas/src/char_set.rs
@@ -1,6 +1,6 @@
-use self::generated::{ID_CHAR_SET, ID_START_CHAR_SET};
-
 mod generated;
+
+use self::generated::{ID_CHAR_SET, ID_START_CHAR_SET};
 
 pub fn is_id_char(c: char) -> bool {
     ID_CHAR_SET.contains_char(c)
