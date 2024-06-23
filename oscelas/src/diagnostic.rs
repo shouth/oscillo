84dacf2f39e08a3c674df94f075aa5804cd889c5
Shota Minami <shouth373@gmail.com>
2024-06-23 21:30:31 +0900

    Update diagnostic implemenation

--- oscelas/src/diagnostic.rs
+++ oscelas/src/diagnostic.rs
@@ -1,6 +1,9 @@
 use std::ops::Range;
+use std::fmt::Write;
 
-use crate::syntax::{OscSyntaxKind, OscSyntaxKindSet};
+use codespan_reporting::diagnostic::{Diagnostic, Label};
+
+use crate::syntax::{OscSyntaxKind::{self, *}, OscSyntaxKindSet};
 
 pub enum SyntaxDiagnostic {
     StrayCharacter {
@@ -14,12 +17,81 @@ pub enum SyntaxDiagnostic {
     },
     IrregularIndentationSequence {
         range: Range<usize>,
-        expected: String,
-        actual: String,
+        previous: Range<usize>,
     },
     UnexpectedToken {
         range: Range<usize>,
         expected: OscSyntaxKindSet,
         actual: OscSyntaxKind,
     },
+}
+
+impl SyntaxDiagnostic {
+    pub fn into_codespan_reporting<FileId: Clone>(self, file_id: &FileId) -> Diagnostic<FileId> {
+        match self {
+            SyntaxDiagnostic::StrayCharacter { range, character } => {
+                Diagnostic::error()
+                    .with_message(format!("stray `{}` in program", character))
+                    .with_labels(vec![
+                        Label::primary(file_id.clone(), range)
+                    ])
+            }
+            SyntaxDiagnostic::IrregularIndentationSize { range, expected, actual } => {
+                Diagnostic::error()
+                    .with_message(format!("expected indentation of width {}, found {}", expected, actual))
+                    .with_labels(vec![
+                        Label::primary(file_id.clone(), range)
+                    ])
+            }
+            SyntaxDiagnostic::IrregularIndentationSequence { range, previous } => {
+                Diagnostic::error()
+                    .with_message(format!("indentation characters are inconsistent"))
+                    .with_labels(vec![
+                        Label::primary(file_id.clone(), range)
+                            .with_message("inconsistent indentation"),
+                        Label::secondary(file_id.clone(), previous)
+                            .with_message("previous indentation"),
+                    ])
+            }
+            SyntaxDiagnostic::UnexpectedToken { range, expected, actual } => {
+                let mut message = String::new();
+                write!(&mut message, "expected ").unwrap();
+                if expected.len() > 1 {
+                    write!(&mut message, "one of ").unwrap();
+                }
+                for (i, kind) in expected.iter().enumerate() {
+                    if i > 0 {
+                        if i + 1 < expected.len() {
+                            write!(&mut message, ", ").unwrap();
+                        } else {
+                            write!(&mut message, " and ").unwrap();
+                        }
+                    }
+
+                    let display_name = match kind.static_token() {
+                        Some(token) => token,
+                        None => match kind {
+                            EOF => "EOF",
+                            INTEGER_LITERAL => "`integer literal`",
+                            FLOAT_LITERAL => "`float literal`",
+                            STRING_LITERAL => "`string literal`",
+                            NEWLINE => "NEWLINE",
+                            INDENT => "INDENT",
+                            DEDENT => "DEDENT",
+                            IDENTIFIER => "`identifier`",
+                            _ => unreachable!(),
+                        }
+                    };
+                    write!(&mut message, "{}", display_name).unwrap();
+                }
+                write!(&mut message, ", found {:?}", actual).unwrap();
+
+                Diagnostic::error()
+                    .with_message(message)
+                    .with_labels(vec![
+                        Label::primary(file_id.clone(), range)
+                    ])
+            }
+        }
+    }
 }
--- oscelas/src/lexer/lexical_analyzer.rs
+++ oscelas/src/lexer/lexical_analyzer.rs
@@ -5,10 +5,33 @@ use super::lookahead::{Lookahead, LookaheadSource};
 use super::LexedToken;
 use super::lexer::Lexer;
 
+pub struct Indentation<'a> {
+    pub text: &'a str,
+    pub offset: usize,
+    pub width: usize,
+}
+
+impl<'a> Indentation<'a> {
+    pub fn new(text: &str, offset: usize) -> Indentation {
+        let text_width = text
+            .chars()
+            .take_while(|c| [' ', '\t'].contains(c))
+            .count();
+
+        let text = &text[..text_width];
+        let width = text
+            .chars()
+            .map(|c| if c == ' ' { 1 } else { 8 })
+            .sum();
+
+        Indentation { text, offset, width }
+    }
+}
+
 pub struct LexicalAnalyzer<'a> {
     source: &'a str,
     inner: Lookahead<Lexer<'a>>,
-    indents: Vec<(&'a str, usize)>,
+    indents: Vec<Indentation<'a>>,
     enclosures: Vec<OscSyntaxKind>,
     is_new_line: bool,
     is_empty_line: bool,
@@ -21,7 +44,7 @@ impl<'a> LexicalAnalyzer<'a> {
         LexicalAnalyzer {
             source,
             inner: Lookahead::new(Lexer::new(source)),
-            indents: vec![("", 0)],
+            indents: vec![Indentation::new("", 0)],
             enclosures: Vec::new(),
             is_new_line: true,
             is_empty_line: false,
@@ -47,7 +70,7 @@ impl<'a> LexicalAnalyzer<'a> {
         if self.is_new_line {
             self.is_new_line = false;
 
-            let (indent, indent_width) = match self.inner.nth(0).kind {
+            let indent = match self.inner.nth(0).kind {
                 TRIVIAL_NEWLINE => {
                     self.is_empty_line = true;
                     return self.next_token();
@@ -58,40 +81,34 @@ impl<'a> LexicalAnalyzer<'a> {
                         return self.inner.bump();
                     }
 
-                    let whitespace = &self.source[self.offset()..][..self.inner.nth(0).length];
-                    let indent_text_width = whitespace
-                        .chars()
-                        .take_while(|c| c == &' ' || c == &'\t')
-                        .count();
-                    let indent = &whitespace[..indent_text_width];
-                    let indent_width = indent.chars().map(|c| if c == '\t' { 8 } else { 1 }).sum();
-                    (indent, indent_width)
+                    let lexeme = &self.source[self.offset()..][..self.inner.nth(0).length];
+                    Indentation::new(lexeme, self.offset())
                 }
-                _ => ("", 0),
+                _ => Indentation::new("", self.offset()),
             };
 
-            let (last_indent, last_indent_width) = self
+            let last_indent = self
                 .indents
                 .last()
                 .expect("indentation stack is empty")
                 .to_owned();
 
-            if indent_width > last_indent_width {
+            if indent.width > last_indent.width {
                 if self.has_indetation_error {
                     self.inner.bump()
                 } else {
-                    self.indents.push((indent, indent_width));
+                    self.indents.push(indent);
                     self.spawn(INDENT)
                 }
-            } else if indent_width < last_indent_width {
+            } else if indent.width < last_indent.width {
                 self.indents.pop();
-                let (_, last_indent_width) = self
+                let last_indent = self
                     .indents
                     .last()
                     .expect("indentation stack is empty")
                     .to_owned();
 
-                if indent_width <= last_indent_width {
+                if indent.width <= last_indent.width {
                     self.is_new_line = true;
                     self.has_indetation_error = false;
                     self.spawn(DEDENT)
@@ -100,21 +117,24 @@ impl<'a> LexicalAnalyzer<'a> {
                     let end = start + self.inner.nth(0).length;
                     self.diagnostics.push(SyntaxDiagnostic::IrregularIndentationSize {
                         range: start..end,
-                        expected: last_indent_width,
-                        actual: indent_width,
+                        expected: last_indent.width,
+                        actual: indent.width,
                     });
                     self.has_indetation_error = true;
                     self.spawn(ERROR)
                 }
-            } else if indent == last_indent {
+            } else if indent.text == last_indent.text {
                 self.inner.bump()
             } else {
                 let start = self.offset();
                 let end = start + self.inner.nth(0).length;
+
+                let last_start = last_indent.offset;
+                let last_end = last_start + last_indent.text.len();
+
                 self.diagnostics.push(SyntaxDiagnostic::IrregularIndentationSequence {
                     range: start..end,
-                    expected: last_indent.to_string(),
-                    actual: indent.to_string(),
+                    previous: last_start..last_end,
                 });
                 self.spawn(ERROR)
             }
